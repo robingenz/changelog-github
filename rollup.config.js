6c545095f0ec9ff38385c7eab92e8050eef6af7b
Robin Genz <mail@robingenz.dev>
2022-03-11 11:15:39 +0100

    fix: npm script `fmt`

--- rollup.config.js
+++ rollup.config.js
@@ -1,11 +1,11 @@
 export default {
-  input: "dist/esm/index.js",
+  input: 'dist/esm/index.js',
   output: {
-    file: "dist/plugin.cjs.js",
-    format: "cjs",
+    file: 'dist/plugin.cjs.js',
+    format: 'cjs',
     sourcemap: true,
     inlineDynamicImports: true,
-    exports: "default",
+    exports: 'default',
   },
-  external: ["dotenv", "@changesets/get-github-info"],
+  external: ['dotenv', '@changesets/get-github-info'],
 };
